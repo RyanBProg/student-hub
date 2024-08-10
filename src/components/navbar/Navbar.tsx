80f74f208f5b79aa8dd93e946eed8de45ba92f70
Ryan <ryanbprog@gmail.com>
2024-08-10 21:41:56 +0100

    dropdowns added for navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,4 +1,6 @@
 import styles from "./navbar.module.scss";
+import { useState, useEffect, useRef, ReactNode } from "react";
+import accountIcon from "/icons/user.png";
 
 export default function Navbar() {
   return (
@@ -6,11 +8,75 @@ export default function Navbar() {
       <p className={styles.logo}>Equinium College</p>
       <nav>
         <ul>
-          <li>Home</li>
           <li>Hub</li>
-          <li>Login</li>
+          <li>News</li>
+          <li>Timetable</li>
+          <Dropdown name={"Social"}>
+            <li>Events</li>
+            <li>Community</li>
+          </Dropdown>
+          <Dropdown
+            name={"Account"}
+            icon={accountIcon}
+            css={styles.dropdown_account}>
+            <li>My Account</li>
+            <li>Messages</li>
+            <li>Invoices</li>
+            <li>Log Out</li>
+          </Dropdown>
         </ul>
       </nav>
     </header>
+  );
+}
+
+type DropdownProps = {
+  children: ReactNode[];
+  name: string;
+  icon: string;
+  css: string;
+};
+
+function Dropdown({ children, name, icon = "", css = "" }: DropdownProps) {
+  const [dropdownIsOpen, setDropdownIsOpen] = useState(false);
+  const dropdownRef = useRef(null);
+  const buttonRef = useRef(null);
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (buttonRef.current && buttonRef.current.contains(event.target)) {
+      return;
+    }
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setDropdownIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    if (dropdownIsOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [dropdownIsOpen]);
+
+  return (
+    <div>
+      <li
+        onClick={() => setDropdownIsOpen((prev) => !prev)}
+        className={styles.account_btn}
+        ref={buttonRef}>
+        {icon && <img src={icon} alt="nav icon" />}
+        {name} &darr;
+      </li>
+      <ul
+        className={`${styles.dropdown} ${
+          dropdownIsOpen && styles.dropdown_open
+        } ${css}`}
+        ref={dropdownRef}>
+        {children}
+      </ul>
+    </div>
   );
 }
